09f4cbc5b0e85882c672db40baeed0f4d753d804
DO-Ubuntu23.10 <1714747596@qq.com>
2024-07-14 15:17:21 +0000

    Lab Page Tables: Print a page table

--- kernel/vm.c
+++ kernel/vm.c
@@ -432,3 +432,31 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+void
+printpte(pte_t pte) {
+  pte_t pa = PTE2PA(pte);
+  printf("pte %p pa %p\n", pte, pa);
+}
+
+// Print pte with its level in the page table tree.
+void vmprint_level(pagetable_t pagetable, int level) {
+  for (int i = 0; i < 512; i++) {
+    pte_t pte = pagetable[i];
+    if (pte & PTE_V) {
+      for (int i = 0; i <= level; i++) {
+        printf(" ..");
+      }
+      printf("%d: ", i);
+      printpte(pte);
+      if (level + 1 <= 2)
+        vmprint_level((pagetable_t)PTE2PA(pte), level + 1);
+    }
+  }
+}
+
+void
+vmprint(pagetable_t pagetable) {
+  printf("page table %p\n", *pagetable);
+  vmprint_level(pagetable, 0);
+}
