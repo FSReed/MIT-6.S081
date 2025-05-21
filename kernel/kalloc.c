b11ddd196a6596665936a03d5c16843e13c3aa16
FSReed <1714747596@qq.com>
2025-05-21 14:34:42 +0800

    Lab locks: 1. Memory Allocator

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -18,15 +18,26 @@ struct run {
   struct run *next;
 };
 
+// NCPU freelists
 struct {
   struct spinlock lock;
   struct run *freelist;
-} kmem;
+  char lockname[10];
+} kmem[NCPU];
 
+char name[10]; // Store lock names
+
+// NOTE: see main(). kinit() only executes if cpuid() == 0!
+// So freerange() only executes once!
+// This step will give all free memories to CPU0!
 void
 kinit()
 {
-  initlock(&kmem.lock, "kmem");
+  for (int i = 0; i < NCPU; i++) {
+    snprintf(kmem[i].lockname, 10, "kmem_CPU%d", i);
+    initlock(&kmem[i].lock, kmem[i].lockname); // Use "kmem", format later.
+  }
+  // Give all free memories to CPU 0
   freerange(end, (void*)PHYSTOP);
 }
 
@@ -56,25 +67,74 @@ kfree(void *pa)
 
   r = (struct run*)pa;
 
-  acquire(&kmem.lock);
-  r->next = kmem.freelist;
-  kmem.freelist = r;
-  release(&kmem.lock);
+  push_off();
+  int id = cpuid();
+  pop_off();
+  acquire(&kmem[id].lock);
+  r->next = kmem[id].freelist;
+  kmem[id].freelist = r;
+  release(&kmem[id].lock);
 }
 
 // Allocate one 4096-byte page of physical memory.
 // Returns a pointer that the kernel can use.
 // Returns 0 if the memory cannot be allocated.
+// Steal memory from other CPUs if this CPU has no free memory
 void *
 kalloc(void)
 {
   struct run *r;
 
-  acquire(&kmem.lock);
-  r = kmem.freelist;
-  if(r)
-    kmem.freelist = r->next;
-  release(&kmem.lock);
+  push_off();
+  int id = cpuid();
+  pop_off();
+  acquire(&kmem[id].lock);
+  r = kmem[id].freelist;
+  if(r) {
+    kmem[id].freelist = r->next;
+    release(&kmem[id].lock);
+  } else {
+    // Steal! First release the current lock in case of deadlock
+    release(&kmem[id].lock);
+    for (int i = 0; i < NCPU; i++) {
+      if (i == id) continue;
+      acquire(&kmem[i].lock);
+      // The following commented code is correct, but it further exacerbates the contention!
+      // Need a better way.
+      // r = kmem[i].freelist;
+      // if (r) {
+      //   kmem[i].freelist = r->next;
+      //   release(&kmem[i].lock);
+      //   break;
+      // }
+      r = kmem[i].freelist;
+      if (r) {
+        // Use Fast-Slow Pointers, to give half of the free memory of this CPU to the current one!
+        struct run *slow = r, *fast = r, *prev = r;
+        while (fast && fast->next) {
+          prev = slow;
+          slow = slow->next;
+          fast = fast->next->next;
+        }
+        // Slow points to the middle of the freelist.
+        // Need to handle the corner case, where another CPU only has one free page.
+        if (slow == prev) {
+          // No free pages for CPU i
+          kmem[i].freelist = 0;
+        } else {
+          prev->next = 0;
+        }
+        release(&kmem[i].lock);
+        // Update current CPU's free memory;
+        r = slow;
+        acquire(&kmem[id].lock);
+        kmem[id].freelist = slow->next;
+        release(&kmem[id].lock);
+        break;
+      }
+      release(&kmem[i].lock);
+    }
+  }
 
   if(r)
     memset((char*)r, 5, PGSIZE); // fill with junk
